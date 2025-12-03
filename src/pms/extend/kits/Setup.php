95d571c68631b683625d89c81c9229adfd8c256f
Guest <kid666@88.com>
2025-12-03 13:07:33 +0800

    优化性能

--- src/pms/extend/kits/Setup.php
+++ src/pms/extend/kits/Setup.php
@@ -3,24 +3,24 @@
 namespace pms\extend\kits;
 
 use pms\contract\LifecycleInterface;
+use pms\facade\BootOptions;
 use pms\facade\Path;
-use pms\program\boot\Options;
 
 class Setup implements LifecycleInterface
 {
 
     protected static string $rootPath;
 
-    public static function entry(string $rootPath, Options $bootOptions): void
+    public static function entry(string $rootPath): void
 	{
 
         static::$rootPath = $rootPath;
-        static::init($bootOptions);
+        static::init();
     }
 
-    protected static function init(Options $bootOptions): void
+    protected static function init(): void
     {
-        $kitsDir = path_join(static::$rootPath, $bootOptions->extend?->kits ?? '/kits');
+        $kitsDir = path_join(static::$rootPath, BootOptions::get_extend('kts','/kits'));
         Path::mount('kitsRoot', $kitsDir);
     }
 
