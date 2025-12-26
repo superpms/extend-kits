662ea45291593302ca094f18573af53280546363
Guest <2434981942@qq.com>
2025-12-26 20:22:06 +0800

    修复bug,完善套件功能

--- src/pms/app/KitsApp.php
+++ src/pms/app/KitsApp.php
@@ -6,14 +6,36 @@ use pms\facade\Path;
 
 trait KitsApp{
 
-    final protected static function path(?string $suffix = null): string{
-        if(!empty($suffix) && !str_starts_with($suffix,"/")){
-            $suffix = "/".$suffix;
+    protected static array $config = [];
+
+    final protected static function path(...$suffix): string{
+        return Path::getKitsRoot(static::getName(),...$suffix);
+    }
+
+    final protected static function getName(): string
+    {
+        $class = get_called_class();
+        $name = explode("\\", $class);
+        $name = array_slice($name, 1, 2);
+        return join('/', $name);
+    }
+
+    final protected static function config(?string $name = null, $default = null){
+        $pluginName = static::getName();
+        if (isset(static::$config[$pluginName])) {
+            $config = static::$config[$pluginName];
+        } else {
+            $configPath = Path::getKitsRoot($pluginName, 'config.php');
+            if (!is_file($configPath)) {
+                return static::$config[$pluginName] = [];
+            }
+            $config = static::$config[$pluginName] = include $configPath;
         }
-        $name = explode("\\",get_called_class());
-        $name = array_slice($name,1,2);
-        $name = implode("\\",$name);
-        return Path::getKitsRoot($name,$suffix);
+        if ($name === null) {
+            return $config;
+        }
+        $data = array_chain($config, $name);
+        return $data !== null ? $data : $default;
     }
 
 }
